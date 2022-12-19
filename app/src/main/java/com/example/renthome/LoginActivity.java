a29d3d2b0bf960ef3a69ef62e93790dde1b2efab
asadbekturgunboyev <murodaka99@gmail.com>
2022-12-20 00:26:44 +0500

    sign_up

--- app/src/main/java/com/example/renthome/LoginActivity.java
+++ app/src/main/java/com/example/renthome/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.renthome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
+    MaterialButton btnEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
+    }
+
+    private void initViews() {
+        btnEnter = findViewById(R.id.btnEnter);
     }
 }
